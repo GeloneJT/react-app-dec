8d4813e4c5f6c5c9c8f4d0f0019954b0b6002232
Jelani <eyeglass0324@gmail.com>
2024-02-07 12:24:30 -0500

    ListGroup index added ListGroup styled using vanilla CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ListGroup from './components/ListGroup/ListGroup';
+import ListGroup from './components/ListGroup';
 
 function App() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
