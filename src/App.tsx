b2fa547fd1b697d6a288e06330ac760eb6ed4cca
Jelani <eyeglass0324@gmail.com>
2024-02-12 13:35:57 -0500

    Form component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useState } from 'react';
-import NavBar from './components/NavBar';
-import Cart from './components/Cart';
+import Form from './components/Form';
 
 function App() {
   const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
 
   return (
     <div>
-      <NavBar cartItemCount={cartItems.length}></NavBar>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
+      <Form></Form>
     </div>
   );
 }
