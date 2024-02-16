f29a1ce726cb31ab7f716a784baedbafd78b31dc
Jelani <eyeglass0324@gmail.com>
2024-02-15 20:37:26 -0500

    Expense miniproject built

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { useState } from 'react';
-import Form from './components/Form';
-
+import { ExpenseList } from './exepense-tracker/components/ExpenseList';
 
 function App() {
-  const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
+    { id: 2, description: 'bbb', amount: 10, category: 'Utilities' },
+    { id: 3, description: 'ccc', amount: 10, category: 'Utilities' },
+    { id: 4, description: 'ddd', amount: 10, category: 'Utilities' },
+  ]);
 
+  if (expenses.length === 0) {
+    return null;
+  }
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      ></ExpenseList>
     </div>
   );
 }
